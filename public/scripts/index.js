86bcf5710a8d600ae08ef9707b7b47a340541d43
Arjee <arjeenunez@gmail.com>
2025-06-01 20:48:22 +0800

    Updated CSS, added the option to opt out of the modal by clicking outside it.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,29 @@
-document.querySelector('.author').addEventListener('click', function (evt) {
-    const button = evt.target.closest('.author__button');
-    if (!button) return;
+'use strict';
+
+const toggleCheck = function (currElement) {
+    console.log(currElement);
     const isMobile = window.matchMedia('(max-width: 800px)').matches;
     if (isMobile) {
-        this.classList.toggle('author--active');
-        button.classList.toggle('author__button--active');
-        this.querySelector('.author__inner').classList.toggle('hidden');
-        this.querySelector('.author__modal').classList.toggle('author__modal--active');
-        this.querySelector('.author__links').classList.toggle('author__links--active');
-        this.querySelector('.author__links--inner').classList.toggle('author__links--inner--active');
+        currElement.querySelector('.author').classList.toggle('author--active');
+        currElement.querySelector('.author__button').classList.toggle('author__button--active');
+        currElement.querySelector('.author__inner').classList.toggle('hidden');
+        currElement.querySelector('.author__modal').classList.toggle('author__modal--active');
+        currElement.querySelector('.author__links').classList.toggle('author__links--active');
+        currElement.querySelector('.author__links--inner').classList.toggle('author__links--inner--active');
     }
+};
+
+window.document.addEventListener('click', function (evt) {
+    let currElement = this.querySelector('.article');
+    const button = evt.target.closest('.author__button');
+    if (!button) {
+        const modal = this.querySelector('.author__links');
+        if (!modal.classList.contains('hidden')) {
+            modal.classList.add('hidden');
+            if (this.querySelector('.author__button').classList.contains('author__button--active')) toggleCheck(currElement);
+        }
+        return;
+    }
+    toggleCheck(currElement);
     this.querySelector('.author__links').classList.toggle('hidden');
 });
